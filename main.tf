ff4ee3bb6091eaa0d6fc6c90155acd295787fcbe
Mmadu Manasseh <mmadu@deimos.co.za>
2020-06-09 09:57:29 +0100

    Added private link endpoint creation

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ terraform {
   }
 }
 
+data azurerm_resource_group "mssql" {
+  name = var.resource_group_name
+}
+
 resource "random_id" "this" {
   byte_length = "8"
 }
@@ -24,13 +28,15 @@ locals {
   vnet_rule_name = var.vnet_rule_name == "" ? "vnet-rule-${var.database_name}" : var.vnet_rule_name
   # Admin login name
   admin_login_name = var.admin_login_name == "" ? "admin-${random_id.this.hex}" : var.admin_login_name
+  # Private endpoint name
+  private_endpoint_name = var.private_endpoint_name == "" ? "private-endpoint-${random_id.this.hex}" : var.private_endpoint_name
 }
 
 # MSSQL Server
 resource "azurerm_mssql_server" "sqlserver" {
   name                          = local.sqlserver_name
-  resource_group_name           = var.resource_group_name
-  location                      = var.location
+  resource_group_name           = data.azurerm_resource_group.mssql.name
+  location                      = data.azurerm_resource_group.mssql.location
   version                       = var.server_version
   administrator_login           = local.admin_login_name
   administrator_login_password  = random_string.this.result
@@ -41,19 +47,36 @@ resource "azurerm_mssql_server" "sqlserver" {
 
 # MSSQL Virtual Network Rule
 resource "azurerm_sql_virtual_network_rule" "sqlvnetrule" {
-  count               = var.subnet_id == "" ? 0 : 1 # Create virtual network rule only if subnet is specified
+  count               = var.create_vnet_rule == false ? 0 : 1
   name                = local.vnet_rule_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = data.azurerm_resource_group.mssql.name
   server_name         = azurerm_mssql_server.sqlserver.name
   subnet_id           = var.subnet_id
 }
-
 
 # MSSQL Database
 resource "azurerm_mssql_database" "mssql_database" {
   name        = var.database_name
   server_id   = azurerm_mssql_server.sqlserver.id
   max_size_gb = var.max_size_gb
+
+  tags = var.tags
+}
+
+# Private Endpoint to connect azure subnets to MSSQL server
+resource "azurerm_private_endpoint" "private_endpoint" {
+  count               = var.create_private_endpoint ? 1 : 0
+  name                = local.private_endpoint_name
+  location            = data.azurerm_resource_group.mssql.location
+  resource_group_name = data.azurerm_resource_group.mssql.name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "privateserviceconnection-${random_id.this.hex}"
+    private_connection_resource_id = azurerm_mssql_server.sqlserver.id
+    subresource_names              = ["sqlServer"]
+    is_manual_connection           = false
+  }
 
   tags = var.tags
 }
--- output.tf
+++ output.tf
@@ -1,6 +1,16 @@
+output "database_name" {
+  description = "The name of the created database"
+  value       = azurerm_mssql_database.mssql_database.name
+}
+
 output "server_administrator_login" {
   description = "Administrator Login username"
   value       = azurerm_mssql_server.sqlserver.administrator_login
+}
+
+output "server_domain_name" {
+  description = "The fully qualified domain name of the Azure SQL Server (e.g. myServerName.database.windows.net"
+  value       = azurerm_mssql_server.sqlserver.fully_qualified_domain_name
 }
 
 output "server_administrator_login_password" {
@@ -18,14 +28,31 @@ output "server_id" {
   value       = azurerm_mssql_server.sqlserver.id
 }
 
-output "subnet_id" {
-  description = "The ID of the subnet that the SQL server is connected to."
-  value       = azurerm_sql_virtual_network_rule.sqlvnetrule.subnet_id
+output "private_link_endpoint_id" {
+  description = "The ID of the private endpoint"
+  value       = azurerm_private_endpoint.private_endpoint[0].id
+
+  depends_on = [azurerm_private_endpoint.private_endpoint]
 }
+
+output "private_link_endpoint_name" {
+  description = "The name of the private endpoint"
+  value       = azurerm_private_endpoint.private_endpoint[0].name
+
+  depends_on = [azurerm_private_endpoint.private_endpoint]
+}
+
+output "private_endpoint_ip_address" {
+  description = "The private IP address associated with the private endpoint"
+  value       = azurerm_private_endpoint.private_endpoint[0].private_ip_address
+
+  depends_on = [azurerm_private_endpoint.private_endpoint]
+}
+
 
 output "vnet_rule_id" {
   description = "The ID of the SQL virtual network rule"
-  value       = azurerm_sql_virtual_network_rule.sqlvnetrule.subnet_id
+  value       = azurerm_sql_virtual_network_rule.sqlvnetrule[0].subnet_id
+
+  depends_on = [azurerm_sql_virtual_network_rule.sqlvnetrule]
 }
-
-
